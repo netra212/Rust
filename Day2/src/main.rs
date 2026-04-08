ada88467a57d1bdd822b5d9fe67eb5760582a6d2
netra212 <netra200021kcbdr@gmail.com>
2026-04-08 23:12:04 +0100

    Control flow in rust

--- Day2/src/main.rs
+++ Day2/src/main.rs
@@ -1,5 +1,7 @@
 // --------------- DAY 2: CONTROL FLOW --------------- //
 
+use core::hash;
+
 fn main() {
     let number = 7;
 
@@ -17,5 +19,36 @@ fn main() {
         if count == 3 {
             break;
         }
+    }
+
+    // while loop.
+    let mut number1 = 3;
+    println!("---- While Loop in Rust ---- ");
+    while number1 > 0 {
+        println!("Number is {number1}");
+        number1 -= 1;
+    }
+
+    // For Loop in rust.
+    for i in 1..=10 {
+        println!("Index i is {i}");
+    }
+
+    // Match statement in Rust.
+    let number2 = 2;
+    match number2 {
+        1 => println!("One "),
+        2 => println!("Two"),
+        3 => println!("Three"),
+        _ => println!("Something else"),
+    }
+
+    let age = 20;
+    let has_id = true;
+
+    if age > 18 && has_id {
+        println!("Eligible for voting");
+    } else {
+        println!("Not Eligible for voting");
     }
 }
