022eb67a35d9cbcc2f44ddc1ddc637b15c298419
netra212 <netra200021kcbdr@gmail.com>
2026-04-09 13:08:49 +0100

    Ownerships in Rust

---
+++ Day3/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let my_string = String::from("Hello, World!");
+    println!("{my_string}");
+
+    takes_ownership(my_string);
+    // my_string is no longer valid here. You cannot use it.
+
+    let x = 5;
+    makes_copy(x);
+    println!("{x}"); // x is still valid (i32 implements Copy)
+
+    // &str (string slice - immutable, stored in binary)
+    let s1: &str = "Hello, World";
+    println!("{s1}");
+
+    // String (heap-allocated, mutable)
+    let mut s2 = String::from("My String");
+    println!("{s2}");
+    s2.push_str(", world");
+    println!("{s2}");
+
+    let s3 = &s2;
+    println!("I borrowed string from s2: {s3}");
+
+    let s4 = s1.to_string();
+    println!("{s4}");
+}
+
+fn takes_ownership(string_value: String) {
+    println!("Takes ownership: {string_value}");
+} // string_value dropped here
+
+fn makes_copy(some_integer: i32) {
+    // Changed name to match usage
+    println!("Value is : {some_integer}");
+}
