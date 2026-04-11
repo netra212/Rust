8c281563e2c28be1463676b9d9efe82e13b4e0e6
netra212 <netra200021kcbdr@gmail.com>
2026-04-11 07:05:34 +0100

    Ownerships and borrowing

--- Day3/src/main.rs
+++ Day3/src/main.rs
@@ -36,7 +36,53 @@ fn main() {
     println!("clone: {clone}");
     println!("original on the stack address, {:p}", &original);
     println!("original on the heap, {:p}", original.as_ptr());
+
+    let king = String::from("Ruler 1");
+    claim_throne(&king);
+    println!("Current King is: {king}");
+
+    let ruler_2 = String::from("Ruler 2");
+    let mut ruler_2 = pass_throne(ruler_2);
+    println("New ruler of empire is: {ruler_2_value}");
+
+    /*
+    Borrowing in Rust.
+    Borrowing allows references to a value without taking ownerships, enabling multiple parts of the code to access the same data without violating Rust's ownerships rules.
+
+    Rules:
+    1. Mulable (read and write) and immutable (read-only) borrowing may not be made to the same value, at the same time.
+    2. A value can either have a single mutiple borrow or unlimited immutable borrows.
+    3. A borrowed reference can not outlive it's scope or the original one.
+    */
+    let mut data = String::from("hello");
+    let ib1 = &data;
+    let ib2 = &data;
+
+    let mb3 = &mut data;
+    // NOTE: mutable and immutable may not be made to the same value, at the same time.
+    println!("ib1: {ib1}, ib2: {ib2}");
+
+    // Defining the scope.
+    {
+        let ib1 = &data;
+        let ib2 = &data;
+        println!("ib1: {ib1}, ib2: {ib2}");
+    } // dropped.
+
+    let mb3 = &mut data;
+    mb3.push_str(", world function..!");
+
+    println!("mb3: {mb3}");
 }
+
+fn pass_throne(name: String) -> String {
+    println!("{name} is passing the Throne");
+    name;
+}
+
+fn claim_throne(name: &String) {
+    println!("{name} is claiming the Throne.");
+} // value will be dropped
 
 fn takes_ownership(string_value: String) {
     println!("Takes ownership: {string_value}");
