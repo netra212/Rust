06961346ebd67ab7fd9d14536d19643f4cca87b6
netra212 <netra200021kcbdr@gmail.com>
2026-05-02 22:28:41 +0100

    iterators in rust

--- Day5/src/main.rs
+++ Day5/src/main.rs
@@ -11,9 +11,32 @@ fn main() {
 
     let result: Vec<_> = iter.collect();
     println!("result: {result:?}");
-}
 
-// pub trait Iterator {
-//     type Item;
-//     fn next(&mut self) --> Option<Self::Item>;
-// }
+    let fruits = ["Apple", "Banana", "Mango"];
+
+    let uppercase_fruits = fruits.iter().map(|&fruit| fruit.to_uppercase());
+
+    println!("Uppercase fruits:");
+
+    for uppercase_fruit in uppercase_fruits {
+        println!("{uppercase_fruit}");
+    }
+
+    let nuts = ["ALMOND", "PEANUTS"];
+
+    let fruits_and_nuts = fruits.iter().chain(&nuts);
+    for f in fruits_and_nuts {
+        println!("Aggregated foods: {f}");
+    }
+
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let words = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let zipped: Vec<_> = numbers.iter().zip(words.iter()).collect();
+
+    for (number, word) in zipped {
+        println!("{number}: {word}");
+    }
+}
