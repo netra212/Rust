0aac682c5e5bfb7229ed100eaa047ecf9fc4b92a
netra212 <netra200021kcbdr@gmail.com>
2026-05-02 20:08:38 +0100

    vectors in rust

---
+++ Day_4/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut numbers: Vec<i32> = Vec::new();
+    println!("Numbers: {numbers:?}");
+
+    numbers.push(4);
+    numbers.push(2);
+    numbers.push(5);
+    println!("Numbers: {numbers:?}");
+
+    let fruits = vec!["Banana", "Apple", "Orange"];
+    println!("Numbers: {fruits:?}");
+}
